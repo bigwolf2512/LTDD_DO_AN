debb5963efec74b761ea5281ba675aca9d5ad754
ThanhHuong <huongthanhthile2001@gmail.com>
2022-12-15 22:51:12 +0700

    list san pham, san pham chi tiet, gio hang

---
+++ app/src/main/java/com/example/do_an/Detail_Product.java
@@ -0,0 +1,14 @@
+package com.example.do_an;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Detail_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_product);
+    }
+}
---
+++ app/src/main/java/com/example/do_an/List_Products.java
@@ -0,0 +1,28 @@
+package com.example.do_an;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class List_Products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_products);
+    }
+    public void user(View view){
+        Intent intent = new Intent(List_Products.this, MainActivity3.class);
+        startActivity(intent);
+    }
+    public void quanly(View view){
+        Intent intent = new Intent(List_Products.this, MainActivity4.class);
+        startActivity(intent);
+    }
+    public void setting(View view){
+        Intent intent = new Intent(List_Products.this, MainActivity5.class);
+        startActivity(intent);
+    }
+}
