e24a81f72a3f8c7aae6ceb3d3e41b4e5a5d9587f
Bao Ngoc <ngocnguyen206201@gmail.com>
2023-01-03 02:20:45 +0700

    login and register

--- app/src/main/java/com/example/do_an/LoginActivity.java
+++ app/src/main/java/com/example/do_an/LoginActivity.java
@@ -1,104 +1,85 @@
 package com.example.do_an;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
-    private FirebaseAuth auth;
-    private Button btnSignup, btnLogin;
-
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        //Get Firebase auth instance
-        auth = FirebaseAuth.getInstance();
+        getSupportActionBar().setTitle("Login");
 
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
-        }
+        editTextLoginEmail = findViewById(R.id.editText_login_email);
+        editTextLoginPwd = findViewById(R.id.editText_login_pwd);
+        progressBar = findViewById(R.id.progressBar);
 
-        // set the view now
-        setContentView(R.layout.login_page);
+        authProfile = FirebaseAuth.getInstance();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        inputEmail = (EditText) findViewById(R.id.editUser);
-        inputPassword = (EditText) findViewById(R.id.editPassword);
-        btnSignup = (Button) findViewById(R.id.button1);
-        btnLogin = (Button) findViewById(R.id.button2);
-
-        //Get Firebase auth instance
-        auth = FirebaseAuth.getInstance();
-
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        //Login User
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = inputEmail.getText().toString();
-                final String password = inputPassword.getText().toString();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
+                if(TextUtils.isEmpty(textEmail)) {
+                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+                    Toast.makeText(LoginActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if(TextUtils.isEmpty(textPwd)) {
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail, textPwd);
                 }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-                //authenticate user
-                auth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                if (!task.isSuccessful()) {
-                                    // there was an error
-                                    if (password.length() < 6) {
-                                        inputPassword.setError(getString(R.string.minimum_password));
-                                    } else {
-                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                                    }
-                                } else {
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }
-                        });
             }
         });
     }
 
-    private void setSupportActionBar(Toolbar toolbar) {
+    private void loginUser(String email, String pwd) {
+        authProfile.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "You are logged in now", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity3.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
